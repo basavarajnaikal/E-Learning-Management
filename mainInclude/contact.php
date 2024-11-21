e67ff22cf1d9d4ff9fb38eebe65eb6724ed03407
basavarajnaikal <basavarajnaikal2491@gmail.com>
2024-11-21 09:49:45 +0530

    Add files via upload

---
+++ mainInclude/contact.php
@@ -0,0 +1,25 @@
+<!-- Start Contact Us -->
+<div class="container mt-4" id="Contact"> <!-- Start Contact Us Container -->
+    <h2 class="text-center mb-4">Contact Us</h2> <!-- Start Contact Us Heading -->
+      <div class="row"> <!-- Start Contact Us Row -->
+        <div class="col-md-8"> <!-- Start Contact Us 1st Column -->
+        <Form action="" method="post">
+          <input type="text" class="form-control" name="name" placeholder="Name"> <br>
+          <input type="text" class="form-control" name="subject" placeholder="Subject"> <br>
+          <input type="text" class="form-control" name="email" placeholder="E-mail"> <br>
+          <textarea class="form-control" name="massage" placeholder="How can we help you" style="height: 150px"></textarea> <br>
+          <input class="btn btn-primary" type="submit" value="Send" name="submit">
+        </Form>
+    </div> <!-- End Contact Us 1st Column -->
+
+  <div class="col-md-4 stripe text-white text-center"> <!-- Start Contact Us 2nd Column -->
+  <h4>Educational Technology</h4>
+  <p>
+    Phone: +91 7829261644 <br />
+    Email: jabez@gmail.com <br>
+  </p>
+  </div> <!-- End Contact Us 2nd Column -->
+ </div> <!-- End Contact Us Row -->
+</div> <!-- Start Contact Us Container -->
+
+<!-- End Contact Us -->
